ec723459d7cc0486237966d826aed510fa31d419
sariGolombeck <149052306+sariGolombeck@users.noreply.github.com>
2024-04-16 08:05:29 +0300

    Update app.component.ts

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,37 +1,4 @@
-// // import { Component, NgModule } from '@angular/core';
-// // import { RouterOutlet } from '@angular/router';
-// // import { CommonModule } from '@angular/common';
-// // import { HeaderComponent } from "./header/header.component";
-// // @Component({
-// //     selector: 'app-root',
-// //     standalone: true,
-// //     templateUrl: './app.component.html',
-// //     styleUrl: './app.component.scss',
-// //     imports: [RouterOutlet, CommonModule, HeaderComponent]
-// // })
-// // export class AppComponent {
-// //   title = 'client';
-// // }
-// import { Component, OnInit } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-// import { HeaderComponent } from './header/header.component';
-// import { RouterModule, Routes } from '@angular/router';
-// import { FooterComponent } from "./footer/footer.component";
-// @Component({
-//     selector: 'app-root',
-//     standalone: true,
-//     templateUrl: './app.component.html',
-//     styleUrl: './app.component.scss',
-//     imports: [RouterOutlet, CommonModule, HeaderComponent, RouterModule, FooterComponent] // Import RouterModule
-// })
-// export class AppComponent implements OnInit{
-//   ngOnInit(): void {
-//    sessionStorage.setItem("token","");
-//   }
 
-//   title = 'EmloyeesManagementSystem';
-// }
 import { CommonModule } from '@angular/common';
 import { Component, AfterViewInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
